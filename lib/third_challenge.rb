2c2f212c267e7edf9b64716ccd7c8b90c7146a0f
bryan lesht <bryan.lesht@colorado.edu>
2020-01-29 20:05:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
       :hero => {name:"Romeo",age:"15",status:"alive"
         #your key/value pairs here
         },
-      :hero_friends => [{:name=>"Benvolio",:age=>"17",:attitude=>"worried"},{:name=>"Mercutio",:age =>"18",:attitude=>"hot-headed"}]
+      :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {name:"Lord Capulet",age:"50"
@@ -22,7 +22,7 @@ def third_challenge
       :heroine => {name:"Juliet",age:"15",status:"alive"
         #your key/value pairs here
         },
-      :heroine_friends => [{:name=>"Steven",:age=>"30",:attitude=>"confused"},{:name=>"Nurse",:age=>"44",:attitude=>"worried"}]
+      :heroine_friends => []
    }
   }
 
@@ -30,4 +30,3 @@ def third_challenge
 end
 
 
-  
